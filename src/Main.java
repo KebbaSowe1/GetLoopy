ba1bcf9e663eed62dda303a1038184de3082b150
Kebba <kebba.sowe24@gmail.com>
2025-03-02 23:37:05 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main{
                 int sum = die1 + die2 + die3;
                 rollCount++;
                 System.out.printf("%4d\t%4d\t%4d\t%4d\t%4d\n", rollCount, die1, die2, die3, sum);
-            } while (!(die1 == die2 && die2 == die3)); // Continue until we get a triple
+            } while (!(die1 == die2 && die2 == die3));
 
             System.out.print("Do you want to roll again? (yes/no): ");
             String response = scanner.next().trim().toLowerCase();
