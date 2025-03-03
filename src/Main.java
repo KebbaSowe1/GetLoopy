3eafdb35ba2c77b50473aede342f66656ef42ec6
Kebba <kebba.sowe24@gmail.com>
2025-03-02 23:37:05 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
-                System.out.print("* ");
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+        boolean keepRolling = true;
+        while (keepRolling) {
+            int rollCount = 0;
+            int die1, die2, die3;
+            System.out.println("Roll\tDie1\tDie2\tDie3\tSum");
+            System.out.println("----------------------------------------------------------");
+            do {
+                die1 = rand.nextInt(6) + 1;
+                die2 = rand.nextInt(6) + 1;
+                die3 = rand.nextInt(6) + 1;
+                int sum = die1 + die2 + die3;
+                rollCount++;
+                System.out.printf("%4d\t%4d\t%4d\t%4d\t%4d\n", rollCount, die1, die2, die3, sum);
+            } while (!(die1 == die2 && die2 == die3)); // Continue until we get a triple
+
+            System.out.print("Do you want to roll again? (yes/no): ");
+            String response = scanner.next().trim().toLowerCase();
+            if (!response.equals("yes")) {
+                keepRolling = false;
             }
-            System.out.println();
         }
+        scanner.close();
     }
 }
